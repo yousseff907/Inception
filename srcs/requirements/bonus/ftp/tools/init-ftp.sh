9af6e3df55979219ec542d6ce63a7bad711da798
Youssef <youssef.itani.11.11@gmail.com>
2025-12-01 01:14:12 +0200

    ftp

--- srcs/requirements/bonus/ftp/tools/init-ftp.sh
+++ srcs/requirements/bonus/ftp/tools/init-ftp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Create user (if it already exists, skip)
 useradd -m "${FTP_USER}" || true
 echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
-chown -R www-data:www-data /var/www/html
+# Give the FTP user access to WordPress files
+usermod -aG www-data "${FTP_USER}"
+
+# Make WordPress directory readable AND writable for the group (www-data)
 chmod -R 775 /var/www/html
+
+# Change the FTP user's home directory
+usermod -d /var/www/html "${FTP_USER}"
 
 exec /usr/sbin/vsftpd /etc/vsftpd.conf

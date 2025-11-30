ec997613f7a4a44bb5d145135a037f4d132fd4a5
Youssef Itani <yitani@c2r6s4.42beirut.com>
2025-11-30 19:27:20 +0200

    working on ftp

--- srcs/requirements/bonus/ftp/tools/init-ftp.sh
+++ srcs/requirements/bonus/ftp/tools/init-ftp.sh
@@ -3,6 +3,7 @@
 useradd -m "${FTP_USER}" || true
 echo "${FTP_USER}:${FTP_PASS}" | chpasswd
 
-chown -R ${FTP_USER}:${FTP_USER} /var/www/html
+chown -R www-data:www-data /var/www/html
+chmod -R 775 /var/www/html
 
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
